cc1cb86cbce7d3df3738706c33d0090d0696825b
Camilo Valencia <acvalencia.contact@gmail.com>
2022-07-25 16:41:16 -0500

    test netlify

--- src/pages/nfts/[address].tsx
+++ src/pages/nfts/[address].tsx
@@ -464,7 +464,7 @@ export default function NftByAddress({
                   <div>
                     <DarkblockAccordion
                       title={`Unlockable Content`}
-                      amount={1}
+                      amount={5}
                       action={() => setDarkblockModalVisibility(true)}
                     >
                       <SolanaDarkblockWidget
